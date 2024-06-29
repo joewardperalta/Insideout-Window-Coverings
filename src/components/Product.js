4d9e0bcf4731faf902a1d6af57c990b7cf1d1476
Joe Peralta <jperalta11@myseneca.ca>
2024-06-29 10:21:01 -0400

    Re-designed products page. Included a category selection and product view

---
+++ src/components/Product.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Product({ imgUrl, title, desc }) {
+  return (
+    <div className="product bg-white rounded">
+      <div className="h-80">
+        <Image
+          className="object-cover w-full h-full rounded-t"
+          src={imgUrl}
+          width={2000}
+          height={300}
+          alt="Product Image"
+        />
+      </div>
+      <div className="p-5">
+        <h6 className="font-semibold mb-3 text-xl">{title}</h6>
+        <p>{desc}</p>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "window-coverings":
+          "linear-gradient(to left, rgba(0,0,0,0.5), rgba(0,0,0,0.7)), url('/blinds_banner-1024x429.jpg')",
+        "window-blinds":
+          "linear-gradient(to left, rgba(0,0,0,0.5), rgba(0,0,0,0.7)), url('/smart-privacy-1-scaled-1.png')",
+        "window-shutters":
+          "linear-gradient(to left, rgba(0,0,0,0.5), rgba(0,0,0,0.7)), url('/window-shutters.jpg')",
+        "window-drapery":
+          "linear-gradient(to left, rgba(0,0,0,0.5), rgba(0,0,0,0.7)), url('/NJRhhmEYBN3kWZsW2GMf6e.jpg')",
+        "window-shades":
+          "linear-gradient(to left, rgba(0,0,0,0.5), rgba(0,0,0,0.7)), url('/types-of-window-treatments-2022-section-7.jpg')",
       },
     },
   },
