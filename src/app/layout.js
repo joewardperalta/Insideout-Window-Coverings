595db39ca4974cd3523d799a6d28007579b93039
Joe Peralta <jperalta11@myseneca.ca>
2024-07-14 23:31:18 -0400

    Added google verification

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,6 +17,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="qg9ONxKPQnyyxycD3XphiFfNA9kPU2mcK9QoUxuPM3M"
+        />
+      </head>
       <body className={poppins.className + " relative"}>
         <NavBar />
         {children}
